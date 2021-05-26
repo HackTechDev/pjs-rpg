69a6a3a34dc69940d2e350060bc537ef316bb938
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2021-05-27 00:39:51 +0200

    [Add] D key

--- js/player.js
+++ js/player.js
@@ -5,63 +5,67 @@
 
 export default class Player {
     constructor(scene, x, y) {
-        this.scene = scene;
+      this.scene = scene;
+        
+
+      const anims = scene.anims;
+      anims.create({
+        key: "misa-left-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-left-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "misa-right-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-right-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "misa-front-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-front-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "misa-back-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-back-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+
+     this.sprite = scene.physics.add
+        .sprite(x, y, "atlas", "misa-front")
+        .setSize(30, 40)
+        .setOffset(10, 24);
+
+    this.keyD = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+    this.keys = scene.input.keyboard.createCursorKeys();
 
 
-  const anims = scene.anims;
-  anims.create({
-    key: "misa-left-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-left-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-  anims.create({
-    key: "misa-right-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-right-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-  anims.create({
-    key: "misa-front-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-front-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-  anims.create({
-    key: "misa-back-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-back-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
+    this.keyDOnce = true;
 
-    this.sprite = scene.physics.add
-    .sprite(x, y, "atlas", "misa-front")
-    .setSize(30, 40)
-    .setOffset(10, 24);
-
-
-
-        this.keys = scene.input.keyboard.createCursorKeys();
     }
 
 
@@ -72,9 +76,23 @@ export default class Player {
 
     update(time, delta) {
         const keys = this.keys;
+        const keyD = this.keyD;
+
         const sprite = this.sprite;
         const speed = 175;
         const prevVelocity = sprite.body.velocity.clone();
+
+        if(keyD.isDown) {
+            
+            if (!this.keyDOnce) {
+                console.log('D key pressed')
+                this.keyDOnce = true;
+            }
+        }
+
+        if(keyD.isUp) {
+          this.keyDOnce = false;
+        }
 
         sprite.body.setVelocity(0);
 
